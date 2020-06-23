97d2caa698533f4e8632eb8d4ef591e2cb08946e
EdwardZack <edwardzack19@gmail.com>
2020-06-23 13:33:56 -0500

    Align_candys v. 2.0
    Generar matriz de recorrido
    Comparar dulces
    Sumar puntos

--- js/app.js
+++ js/app.js
@@ -46,17 +46,16 @@ function cargarImagenes(){
 }
 
 function asignarEventos(){
+  addClassElemento();
   for(var i=1; i<8; i++){
     for(var j=1; j<8; j++){
       if(i==1){
         numColNext = i+1;
         $k('.col-'+i).sortable({
           update: function(){
-            addClassElemento();
             asignarEventos();
           }
         });
-
         $k('.col-'+i+' img:nth-child('+j+')').droppable({
           accept: '.col-'+numColNext+' img:nth-child('+j+')',
           drop: intercambiarDulces
@@ -68,7 +67,6 @@ function asignarEventos(){
         $k('.col-'+numColNext+' img:nth-child('+j+')').addClass('item'+i+j);
         $k('.col-'+i).sortable({
           update: function(){
-            addClassElemento();
             asignarEventos();
           }
         });
@@ -80,11 +78,9 @@ function asignarEventos(){
         numColPrev = i-1;
         $k('.col-'+i).sortable({
           update: function(){
-            addClassElemento();
             asignarEventos();
           }
         });
-
         $k('.col-'+i+' img:nth-child('+j+')').droppable({
           accept: '.col-'+numColPrev+' img:nth-child('+j+')',
           drop: intercambiarDulces
@@ -92,21 +88,196 @@ function asignarEventos(){
       }
     }
   }
+  habilitarSorDrop();
 }
 
+function habilitarSorDrop(){
+  for(var i=1; i<8; i++){
+    $k('.col-'+i).sortable('enable');
+  }
+  $k('img').droppable('enable');
+}
+
+function deshabilitarSorDrop(){
+  for(var i=1; i<8; i++){
+    $k('.col-'+i).sortable('disable');
+  }
+  $k('img').droppable('disable');
+}
+
+function generarMatrizDulces(){
+  var matrizDulces = [];
+
+  for(var i=1; i<8; i++){
+    var colDulces = $k('.col-'+i).children();
+    matrizDulces.push(colDulces);
+  }
+  return matrizDulces;
+}
+
+function validarColumnas(){
+  var totalColDulces = [];
+  for(var i=0; i<7; i++){
+    var contadorCol = 0;
+    var firstColMatch = [];
+    var secondColMatch = [];
+    var matrizDulces = generarMatrizDulces();
+    var colDulceSelect = [];
+    var colDulceNextSelect = [];
+    var continueCol = false;
+    for (var j=1; j<7; j++){
+      if(colDulceSelect.length == 0){
+        colDulceSelect = matrizDulces[i].eq(0);
+        rowDulceSelect = matrizDulces[0].eq(j);
+
+      };
+    var colDulceNextSelect = matrizDulces[i].eq(j);
+    var srcColInicial = colDulceSelect.attr('src');
+    var srcColDulceNextSelect = colDulceNextSelect.attr('src');
+
+    if(srcColInicial != srcColDulceNextSelect){
+      if(firstColMatch.length >= 3){
+        continueCol = true;
+      } else {
+        firstColMatch = [];
+      }
+      contadorCol = 0;
+    }else {
+      if(contadorCol == 0){
+        if(!continueCol){
+          firstColMatch.push(colDulceSelect);
+        } else {
+          secondColMatch.push(colDulceSelect);
+        }
+      }
+      if(!continueCol){
+        firstColMatch.push(colDulceNextSelect);
+      } else {
+        secondColMatch.push(colDulceNextSelect);
+      }
+      contadorCol++;
+    }
+    colDulceSelect = matrizDulces[i].eq(j);
+  }
+  if (secondColMatch.length > 2){
+    firstColMatch = $k.merge(firstColMatch, secondColMatch);
+  }
+  if(firstColMatch.length <= 2){
+    firstColMatch = [];
+  }
+  if(firstColMatch.length >=3){
+    sumarPuntos(firstColMatch);
+  }
+  totalColDulces = $k.merge(totalColDulces, firstColMatch);
+  }
+  if(totalColDulces.length >= 3){
+    marcarDulces(totalColDulces);
+  }
+}
+
+function validarFilas(){
+  var totalRowDulces = [];
+  for(var i=0; i<7; i++){
+    var contadorRow = 0;
+    var firstRowMatch = [];
+    var secondRowMatch = [];
+    var matrizDulces = generarMatrizDulces();
+    var rowDulceSelect = [];
+    var rowDulceNextSelect = [];
+    var continueRow = false;
+    for (var j=1; j<7; j++){
+      if(rowDulceSelect.length == 0){
+        rowDulceSelect = matrizDulces[0].eq(i);
+      };
+    var rowDulceNextSelect = matrizDulces[j].eq(i);
+    var srcRowInicial = rowDulceSelect.attr('src');
+    var srcRowDulceNextSelect = rowDulceNextSelect.attr('src');
+
+    if(srcRowInicial != srcRowDulceNextSelect){
+      if(firstRowMatch.length >= 3){
+        continueRow = true;
+      } else {
+        firstRowMatch = [];
+      }
+      contadorRow = 0;
+    }else {
+      if(contadorRow == 0){
+        if(!continueRow){
+          firstRowMatch.push(rowDulceSelect);
+        } else {
+          secondRowMatch.push(rowDulceSelect);
+        }
+      }
+      if(!continueRow){
+        firstRowMatch.push(rowDulceNextSelect);
+      } else {
+        secondRowMatch.push(rowDulceNextSelect);
+      }
+      contadorRow++;
+    }
+    rowDulceSelect = matrizDulces[j].eq(i);
+  }
+  if (secondRowMatch.length > 2){
+    firstRowMatch = $k.merge(firstRowMatch, secondRowMatch);
+  }
+  if(firstRowMatch.length <= 2){
+    firstRowMatch = [];
+  }
+  if(firstRowMatch.length >= 3){
+    sumarPuntos(firstRowMatch);
+  }
+  totalRowDulces = $k.merge(totalRowDulces, firstRowMatch);
+  }
+  if(totalRowDulces.length >= 3){
+    marcarDulces(totalRowDulces);
+  }
+}
+
+function sumarPuntos(totalDulces){
+  var totalLinea = totalDulces.length;
+  var totalPuntos = Number($k('#score-text').text());
+  switch (totalLinea) {
+    case 3:
+      totalPuntos += 15;
+      break;
+    case 4:
+      totalPuntos += 40;
+      break;
+    case 5:
+      totalPuntos += 75;
+      break;
+    case 6:
+      totalPuntos += 120;
+      break;
+    case 7:
+      totalPuntos += 175;
+  }
+  $k('#score-text').text(totalPuntos);
+}
+
+function marcarDulces(totalDulces){
+  for(i=0; i<totalDulces.length; i++){
+    totalDulces[i].addClass('borrar');
+  }
+}
+
+function borrarDulces(){
+  validarColumnas();
+  validarFilas();
+  if($k('.borrar').length !=0){
+    $k('.borrar').remove();
+    asignarEventos();
+  }
+}
 
 function cargarTablero(){
   for(var i = 1; i<8; i++){
     $k('.panel-tablero div:nth-child('+i+')').html(cargarImagenes());
   }
+  $k('.panel-tablero div').disableSelection();
+  $k('#score-text').text('0');
   addClassElemento();
   asignarEventos();
-
-
-$k('.panel-tablero div').disableSelection();
-
-
-
 }
 
 function intercambiarDulces(event, imgDrag){
@@ -122,15 +293,11 @@ function addClassElemento(){
   $k('.panel-tablero div').children().removeClass().addClass('elemento');
 }
 
-function mostrarFilas(){
-  var filas = [$k('.col-1 img:nth-child(1)'), $k('.col-2 img:nth-child(1)')];
-}
-
 $k(function(){
   colorMatch($k(".main-titulo"));
   cargarTablero();
+  borrarDulces();
   $k('.btn-reinicio').click(function(){
     $k('#timer').startTimer();
   });
-  mostrarFilas();
 })
